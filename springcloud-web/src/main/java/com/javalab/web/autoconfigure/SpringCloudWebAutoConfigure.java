60bd7813f5dde40e67dd70f05438a4517c5329c9
javalab <mazan_abdelkader@yahoo.de>
2021-04-27 19:14:58 +0200

    Service Discovery Demo using Zookeeper and Spring Cloud

---
+++ springcloud-web/src/main/java/com/javalab/web/autoconfigure/SpringCloudWebAutoConfigure.java
@@ -0,0 +1,13 @@
+package com.javalab.web.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudWebAutoConfigure {
+
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public ServicesController servicesController(ServiceRegistry serviceRegistry){
+//        return new ServicesController(serviceRegistry);
+//    }
+}

a1d4bca3fb8302b14dbfd335dd585d04ce741347
javalab <mazan_abdelkader@yahoo.de>
2021-05-01 18:54:27 +0200

    added missing autoconfiguration

--- springcloud-web/src/main/java/com/javalab/web/autoconfigure/SpringCloudWebAutoConfigure.java
+++ springcloud-web/src/main/java/com/javalab/web/autoconfigure/SpringCloudWebAutoConfigure.java
@@ -1,13 +1,17 @@
 package com.javalab.web.autoconfigure;
 
+import com.javalab.springcloud.registry.ServiceRegistry;
+import com.javalab.web.controller.ServicesController;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringCloudWebAutoConfigure {
 
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public ServicesController servicesController(ServiceRegistry serviceRegistry){
-//        return new ServicesController(serviceRegistry);
-//    }
+    @Bean
+    @ConditionalOnMissingBean
+    public ServicesController servicesController(ServiceRegistry serviceRegistry){
+        return new ServicesController(serviceRegistry);
+    }
 }
